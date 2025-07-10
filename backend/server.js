4674b0e0af47be57abf6caa4737d71b70af49a87
Rahil <rahilpatial161381@gmail.com>
2025-07-10 06:36:37 +0530

    Created CriketTechnologies.js, FootballTechnologies.js, TennisTechnologies.js and worked on server.js,

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+//const fetch = require('node-fetch');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
